24277215a77f9bcf681ac6a8bc8cb55ad1e63bd4
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-24 20:46:52 -0600

    Diseño basico parte 2

--- disenos/lib/main.dart
+++ disenos/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:disenos/screens/basic_design.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,15 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      initialRoute: 'basic_design',
+      routes: {
+        'basic_design':( _ ) => BasicDesignScreen()
+      },
+      
     );
   }
 }
+
