8e66a3864d3c3729687546ad5d7e7108f6073748
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-25 20:01:29 -0600

    Diseño Compuesto, Fondos y Gradientes

---
+++ disenos/lib/screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:disenos/widgets/background.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+   
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Stack(
+        children: [
+          //Background
+          Background(),
+        ],
+         
+
+
+      )
+    );
+  }
+}
