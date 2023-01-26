9f65e4cb2962d7f21a4a4d416767be108895d21b
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-01-25 20:17:47 -0600

    Diseño Statusbar y titulos

--- disenos/lib/main.dart
+++ disenos/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:disenos/screens/basic_design.dart';
 import 'package:disenos/screens/home_screen.dart';
 import 'package:disenos/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //Cambiar de dark/light el status bar
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
--- disenos/lib/screens/home_screen.dart
+++ disenos/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:disenos/widgets/background.dart';
+import 'package:disenos/widgets/page_title.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,11 +11,34 @@ class HomeScreen extends StatelessWidget {
         children: [
           //Background
           Background(),
+          // Home Body
+          _HomeBody()
         ],
          
 
 
       )
+    );
+  }
+}
+
+
+class _HomeBody extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+
+          // Titulos
+          PageTitle(),
+
+          // Card Table
+          // CardTable(),
+        ],
+      ),
     );
   }
 }
